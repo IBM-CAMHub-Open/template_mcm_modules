ff4332d706a971332f02dfd3f7f1676978ad2016
CAMHub PublishBot <octravis@us.ibm.com>
2019-01-30 19:58:53 +0000

    Automated CAMHub::publish on 2019-01-30 19:58:53 UTC

--- config_mcm_download/variables.tf
+++ config_mcm_download/variables.tf
@@ -1,6 +1,6 @@
 variable "vm_os_password"       { type = "string"  description = "Operating System Password for the Operating System User to access virtual machine"}
 variable "vm_os_user"           { type = "string"  description = "Operating System user for the Operating System User to access virtual machine"}
-variable "boot_ipv4_address"    { type="string"      description = "IPv4 Address's in List format"}
+
 variable "master_ipv4_address"  { type="string"      description = "Master Node IPv4 Address's"}
 variable "private_key"          { type = "string"  description = "Private SSH key Details to the Virtual machine"}
 variable "random"               { type = "string"  description = "Random String Generated"}
